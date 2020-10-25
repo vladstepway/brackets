ed66ea53cb933d06a8d79dcdb104a9cffa6f6d34
ustsepavy <vladstepovoyvios@gmail.com>
2020-10-25 19:44:39 +0300

    feat: brackets task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let previousStr = '';
+    while (str) {
+      for (let i = 0; i < bracketsConfig.length; i++) {
+        str = str.replace(bracketsConfig[i].join(''), '');
+      }
+      if (str === previousStr) {
+        break;
+      }
+      previousStr = str;
+    }
+    return str.length === 0;
+  }
